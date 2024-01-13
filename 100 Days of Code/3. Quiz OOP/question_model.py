39c321e79597ee32fed01dfd055e4a7f0f9098ec
JRT-Vgz <141759664+JRT-Vgz@users.noreply.github.com>
2024-01-13 18:14:29 +0100

    Delete 100 Days of Code directory

--- 100 Days of Code/3. Quiz OOP/question_model.py
+++
@@ -1,7 +0,0 @@
-
-# Crea la clase Question, para convertir en un objeto cada pregunta del juego.
-class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
-        
